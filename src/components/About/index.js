092ec94ec360f905d3b67592450b8de6a35779a0
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-17 15:33:32 -0400

    About and Navigation component started

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+    return(
+    <section className="title">
+      <h1 class="name">About: Nathan Nitschke</h1>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+            <img class="mb-5" src="https://github.com/Nathan1133/react-portfolio-nathan/raw/master/src/assets/images/profile-pic1.png" alt="Nathan Nitschke"/>
+        <p>
+            I'm baby enamel pin subway tile 90's green juice iPhone you probably haven't heard of them skateboard plaid chillwave pickled kickstarter mixtape. Fanny pack four dollar toast celiac vape. Poutine jianbing pour-over, pickled next level yr gentrify helvetica gluten-free occupy tattooed 3 wolf moon schlitz locavore wolf. Food truck vice pok pok forage vinyl master cleanse pug chartreuse asymmetrical. IPhone four dollar toast four loko +1, before they sold out pok pok yr cray post-ironic pitchfork tacos schlitz aesthetic literally.
+        </p>
+       
+       
+      <p>
+            View full <a href="#/resume" class="link">resume</a>
+    </p>
+      </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;
